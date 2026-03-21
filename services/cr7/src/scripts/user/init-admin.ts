d122ba57759483ed0b1666282621f464ebd19105
ghostiee <y_jw@outlook.com>
2026-03-21 18:14:33 +0800

    feat: update init-admin command to make password optional and improve error handling

--- services/cr7/src/scripts/user/init-admin.ts
+++ services/cr7/src/scripts/user/init-admin.ts
@@ -25,8 +25,8 @@ export const builder = {
   },
   password: {
     type: 'string',
-    demandOption: true,
-    describe: '登录密码'
+    demandOption: false,
+    describe: '登录密码（首次设置管理员密码时必填）'
   },
   schema: {
     type: 'string',
@@ -41,22 +41,33 @@ export async function initAdmin(
     schema: string;
     countryCode?: string;
     phone: string;
-    password: string;
+    password?: string;
   }
 ) {
   const {
     schema,
     countryCode = '+86',
     phone,
-    password,
+    password = null,
   } = args;
 
   const uid = await upsertUserByPhone(client, schema, countryCode, phone, 'system admin');
 
   const roleId = await getRoleIdByName(client, schema, 'ADMIN');
 
-  await assignRoleToUser(client, schema, uid, roleId)
-    .then(() => upsertUserPassword(client, schema, uid, password));
+  await assignRoleToUser(client, schema, uid, roleId);
+
+  const userProfile = await getUserProfile(client, schema, uid);
+  const passwordExists = userProfile.auth_methods.includes('PASSWORD');
+  const passwordLen = password?.length ?? 0;
+
+  if (passwordLen === 0 && passwordExists === false) {
+    throw new Error('管理员账号尚未设置密码，请通过 --password 指定初始密码');
+  }
+
+  if (passwordLen > 0) {
+    await upsertUserPassword(client, schema, uid, password);
+  }
 
   return getUserProfile(client, schema, uid);
 }
